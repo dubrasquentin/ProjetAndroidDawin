80cd35a013e5c3940c63d009cb9146830df3b555
dubrasquentin <dubrasquentin@gmail.com>
2018-01-25 09:01:14 +0100

    Create Intent

--- app/src/main/java/com/example/cfrecheville/projetdawinmobile/MainActivity.java
+++ app/src/main/java/com/example/cfrecheville/projetdawinmobile/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.cfrecheville.projetdawinmobile;
 
+import android.content.Intent;
 import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,25 +22,37 @@ public class MainActivity extends AppCompatActivity {
         ImageButton ib3 = (ImageButton) findViewById(R.id.imgLoutre);
 
 
-        ib.setOnClickListener(new View.OnClickListener(){
+        ib.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 choice = 1;
             }
         });
 
-        ib2.setOnClickListener(new View.OnClickListener(){
+        ib2.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 choice = 2;
             }
         });
 
-        ib3.setOnClickListener(new View.OnClickListener(){
+        ib3.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 choice = 3;
+            }
+        });
+        final Button Active = (Button) findViewById(R.id.Active);
+        Active.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, GameActivity.class);
+                intent.putExtra("choice", choice);
+                intent.putExtra("tailleimg", tailleImg);
+                startActivity(intent);
             }
         });
     }
 }
+
